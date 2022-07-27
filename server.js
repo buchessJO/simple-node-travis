08691b4b335b58019d2b259b8d4afbe24b0d7473
srv1 <nnebuchi.j.osigbo@gmail.com>
2022-07-27 22:12:40 +0100

    configuting env vsr in travis dashboard

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const FAVOURITE_INSTRUCTOR = process.env.NAME;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`${FAVOURITE_INSTRUCTOR} rocks!`)
     await sleep(5000);
   }
 }
